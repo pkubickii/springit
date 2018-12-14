19eae030f9f7e72cba81198031c760031994f9b9
Przemyslaw <kubicki.przemyslaw@gmail.com>
2018-12-14 13:09:00 +0100

    [change] controller MyController + me.html

--- src/main/java/pl/pkubicki/git/springit/controller/MyController.java
+++ src/main/java/pl/pkubicki/git/springit/controller/MyController.java
@@ -1,6 +1,7 @@
 package pl.pkubicki.git.springit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,11 @@ public class MyController {
     @GetMapping({"/", "/home"})
     public String welcome() {
         return "welcome";
+    }
+    @GetMapping("/me")
+    public String me(Model model) {
+        model.addAttribute("name", "Przemysław");
+        model.addAttribute("surname", "Kubicki");
+        return "me";
     }
 }
