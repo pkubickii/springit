092ef0e54cb4caba8d428b181b0988ce1efc1786
Przemyslaw <kubicki.przemyslaw@gmail.com>
2018-12-14 12:19:26 +0100

    [add] controller MyController + welcome.html

---
+++ src/main/java/pl/pkubicki/git/springit/controller/MyController.java
@@ -0,0 +1,12 @@
+package pl.pkubicki.git.springit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping({"/", "/home"})
+    public String welcome() {
+        return "welcome";
+    }
+}
