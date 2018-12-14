73633572accee963f8151b2cf8fe32559a1be2c7
Przemyslaw <kubicki.przemyslaw@gmail.com>
2018-12-14 12:00:57 +0100

    [init] initial commit

---
+++ src/main/java/pl/pkubicki/git/springit/SpringitApplication.java
@@ -0,0 +1,14 @@
+package pl.pkubicki.git.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitApplication.class, args);
+    }
+
+}
+
